1bedc0e9d7fec73619c3aae6234e10283ef5f985
Raphael F <raphael.fontes@ifs.edu.br>
2024-03-29 22:34:26 -0300

    feat: base entidades router

--- apps/base/routers.py
+++ apps/base/routers.py
@@ -1,9 +1,9 @@
 from rest_framework.routers import DefaultRouter
-from apps.sig.routers import router as sig_router
-from apps.sig.viewsets import PessoaViewSet
+
+from apps.base.viewsets import EntidadeViewSet
 from apps.validador.routers import router as validador_routers
 
 router = DefaultRouter()
-# router.register(r'pessoa', PessoaViewSet, basename='pessoa')
+router.register(r'entidades', EntidadeViewSet, basename='entidades')
 
 router.registry.extend(validador_routers.registry)
