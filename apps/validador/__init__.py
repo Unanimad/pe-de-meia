bc4fbdd42fa10a82e72e800215fcd8d7c9325d22
Raphael F <raphael.fontes@ifs.edu.br>
2024-03-30 13:44:11 -0300

    add: abs class validador pe de meia + parse estudante

--- apps/validador/__init__.py
+++ apps/validador/__init__.py
@@ -1,3 +1,4 @@
+from abc import ABC
 from typing import Dict
 
 from django.db.models import QuerySet
@@ -7,7 +8,27 @@ Portaria: https://www.in.gov.br/web/dou/-/portaria-n-84-de-7-de-fevereiro-de-202
 """
 
 
-class PeMeiaEstudante:
+class PeMeia(ABC):
+    cols = {}
+
+    def __init__(self, qs: QuerySet):
+        self.qs = qs
+
+    @classmethod
+    def _ha_registros(cls, qs: QuerySet):
+        return qs.count()
+
+    def valide(self) -> Dict:
+        valida_methods = [func.replace('valida_', '') for func in dir(self) if callable(getattr(self, func))
+                          if func.startswith('valida')]
+
+        return {method: getattr(self, f'valida_{method}')() for method in valida_methods}
+
+    def gera_csv(self):
+        ...
+
+
+class PeMeiaEstudante(PeMeia):
     """
     Campos disponiveis no manual https://www.gov.br/mec/pt-br/pe-de-meia/manualSGP.pdf
     """
@@ -22,33 +43,33 @@ class PeMeiaEstudante:
         'MATRICULA_ATE_DOIS_MESES_DO_INICIO'
     }
 
-    def __init__(self, qs: QuerySet):
-        self.qs = qs
-
-    @classmethod
-    def __ha_registros(cls, qs: QuerySet):
-        return qs.count()
-
-    def valide(self) -> Dict:
-        valida_methods = [func.replace('valida_', '') for func in dir(self) if callable(getattr(self, func))
-                          if func.startswith('valida')]
-
-        return {method: getattr(self, f'valida_{method}')() for method in valida_methods}
-
     def valida_cpfs_nulos(self):
         qs = self.qs.filter(cpf__isnull=True)
-        return self.__ha_registros(qs)
+        return self._ha_registros(qs)
 
     def valida_nomes_maes_nulos(self):
         qs = self.qs.filter(nome_mae__isnull=True)
-        return self.__ha_registros(qs)
+        return self._ha_registros(qs)
 
     def valida_menores_anos(self, idade: int = 14):
         qs = self.qs.filter(idade__lt=idade)
-        return self.__ha_registros(qs)
+        return self._ha_registros(qs)
+
+    @classmethod
+    def parse(cls, estudante: Dict, type_='csv') -> Dict:
+        return {
+            'ESTUDANTE_CPF': estudante.get('cpf', ''),
+            'ESTUDANTE_NOME': estudante.get('nome', ''),
+            'ESTUDANTE_DT_NASCIMENTO': estudante.get('data_nascimento', ''),
+            'ESTUDANTE_MAE_NOME': estudante.get('nome_mae', ''),
+            'CO_ENTIDADE': estudante.get('', ''),
+            'NO_ENTIDADE': estudante.get('', ''),
+            'ESTUDANTE_ETAPA DE ENSINO': estudante.get('etapa_ensino', ''),
+            'MATRICULA_ATE_DOIS_MESES_DO_INICIO': estudante.get('', ''),
+        }
 
 
-class PeMeiaFrequencia:
+class PeMeiaFrequencia(PeMeia):
     """
     Manual: https://www.gov.br/mec/pt-br/pe-de-meia/manual-de-envio-dos-dados-ao-sistema-presente-frequencia
 
@@ -72,7 +93,7 @@ class PeMeiaFrequencia:
     }
 
 
-class PeMeiaConclusao:
+class PeMeiaConclusao(PeMeia):
     """
     Conclusão da série em que está matriculado, com aprovação e, quando for o caso,
     participação nos exames do Saeb, e nos exames aplicados pelos sistemas de avaliação
@@ -81,7 +102,7 @@ class PeMeiaConclusao:
     ...
 
 
-class PeMeiaEnem:
+class PeMeiaEnem(PeMeia):
     """
     Inscrição e participação nos dois dias de realização do Exame Nacional do Ensino Médio - Enem,
     incluindo eventual reaplicação nas situações de excepcionalidade
