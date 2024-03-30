c8708ce5283fdbaa48f2d666b65458b471a70b24
Raphael F <raphael.fontes@ifs.edu.br>
2024-03-30 07:55:48 -0300

    add: frontend next app npx

---
+++ frontend/app/page.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+import {lusitana} from '@/app/ui/fonts'
+
+
+export default function Page() {
+    return (
+        <main className="flex min-h-screen flex-col p-6">
+
+        </main>
+    );
+}
