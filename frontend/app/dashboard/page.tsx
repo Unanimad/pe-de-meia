518ead9dcade8f6c3bbc535bf91b91e64e130314
Raphael F <raphael.fontes@ifs.edu.br>
2024-03-30 13:44:49 -0300

    feat: download csv

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1,6 +1,14 @@
 import {EntidadesSelect} from "@/components/ui/combobox";
 import {DataTable} from "@/app/dashboard/data-table";
 import {columns} from "./columns"
+import {CloudDownloadIcon} from "lucide-react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
+import Link from 'next/link'
 
 async function getData(searchParams) {
   let url = 'http://localhost:8000/api/estudantes/?' + new URLSearchParams(searchParams)
@@ -35,7 +43,7 @@ export default async function Page({searchParams}) {
     <section className={'flex flex-row space-x-20 mt-10'}>
       <div className={'basis-1/8'}>
         <div className={'mb-5'}>
-          <h2>Informações</h2>
+          <h2>Geral</h2>
         </div>
 
         <div className={'p-4 mb-5 rounded-lg shadow info'}>
@@ -54,7 +62,25 @@ export default async function Page({searchParams}) {
       </div>
       <div className={'grow'}>
         <div className={'mb-5'}>
-          <h2>Discentes</h2>
+          <div className={'grid grid-cols-2'}>
+            <div>
+              <h2>Discentes</h2>
+            </div>
+            <div className={'text-right'}>
+              <span>
+                <TooltipProvider>
+                  <Tooltip>
+                    <TooltipTrigger>
+                       <Link href="http://localhost:8000/api/estudantes/download_csv/"> <CloudDownloadIcon
+                         size={22}/></Link></TooltipTrigger>
+                    <TooltipContent>
+                      <p>Fazer download da planilha.</p>
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
+              </span>
+            </div>
+          </div>
         </div>
         <DataTable columns={columns} data={data['results']}/>
       </div>
