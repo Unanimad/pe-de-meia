3d5cc21a7b909dc02bb2c2e0cc2039dfee6bae24
Raphael F <raphael.fontes@ifs.edu.br>
2024-03-30 09:31:34 -0300

    add: frontend dashboard requisicao e apresentacao das validacoes

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1,30 +1,40 @@
 // <!-- text-orange-500 text-emerald-500 -->
 
+async function getData() {
+    const res = await fetch('http://localhost:8000/api/estudantes/?format=json')
+    if (!res.ok) {
+        throw new Error('Failed to fetch data')
+    }
+    return res.json()
+}
 
-export default function Page() {
+
+export default async function Page() {
+
+    const data = await getData();
+
     return <>
         <h1>Dashboard Page!</h1>
         <section className={'flex flex-row space-x-4 mt-10'}>
-            <div className={'basis-1/4 rounded  '}>
+            <div className={'basis-1/4'}>
                 <div className={'mb-5'}>
                     <h2>Informações</h2>
                 </div>
-                <div className={'p-4 divide-y shadow-xl info'}>
-                    <div>
-                        <span>ljkadsjlks</span>
-                        <h3>0</h3>
-                    </div>
-                    <div>
-                        <span>ljkadsjlks</span>
-                    </div>
-                    <div>
-                        <span>ljkadsjlks</span>
-                    </div>
+                <div className={'divide-y rounded-lg shadow info'}>
+                    {data['info'] && Object.entries(data['info']).map(([key, value], i) => (
+                        <div key={i}>
+                            <span>{key}</span>
+                            <h3 className={value === 0 ? 'text-emerald-500' : 'text-orange-500'}>{value}</h3>
+                        </div>
+                    ))}
                 </div>
             </div>
-            <div className={'basis-1/2'}>
-                <div className={'rounded shadow p-4'}>
+            <div className={'basis-1/2 '}>
+                <div className={'mb-5'}>
                     <h2>Discentes</h2>
+                </div>
+                <div className={'rounded shadow p-4'}>
+
                 </div>
             </div>
         </section>
