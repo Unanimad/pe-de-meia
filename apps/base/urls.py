ed9d16635a65949af7a49b4cdb72159150175325
Raphael F <raphaelf.ti@gmail.com>
2024-04-29 14:27:03 -0300

    add index html view

--- apps/base/urls.py
+++ apps/base/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 
 from apps.base.routers import router
+from apps.base.views import index
 
 app_name = 'base'
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', index, name='index'),
+    path('api/', include(router.urls)),
     path('auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
